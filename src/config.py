49980ff1401bd921fe80c44bc25392a5528abfc5
practica <prueba@gmail.com>
2026-07-30 20:37:48 -0600

    Conectar la API con la base de datos de Railway

--- src/config.py
+++ src/config.py
@@ -2,15 +2,11 @@ import os
 import mysql.connector
 
 def get_db_connection():
-    """
-    Establece y retorna una conexión a la base de datos MySQL.
-    Utiliza variables de entorno (útiles para Railway) y respaldo local si no existen.
-    """
     connection = mysql.connector.connect(
-        host=os.getenv('MYSQLHOST', 'localhost'),
-        user=os.getenv('MYSQLUSER', 'root'),
-        password=os.getenv('MYSQLPASSWORD', '242716'),  # Tu contraseña local actual
-        database=os.getenv('MYSQLDATABASE', 'veci_alerta_db'),
-        port=int(os.getenv('MYSQLPORT', 3306))
+        host="nozomi.proxy.rlwy.net",
+        user="root",
+        password="VMDIugKDMGnoadbGVhvWhvfbAWKBzgbn",
+        database="railway",
+        port=12938
     )
     return connection
